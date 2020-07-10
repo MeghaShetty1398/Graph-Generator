b0d0d3e4cf924b7b448c982c96d4923ac65db3a6
Megha <shettymegha098@gmail.com>
2020-07-10 00:26:14 -0700

    Modified Login page and added Registration Page

--- login.js
+++ login.js
@@ -21,14 +21,38 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/register.html'));
+});
+app.post('/register-user', function(request, response) {
+	var email = request.body.email;
+	var username = request.body.username;
+	var password = request.body.password;
+	var  sql = "SELECT USERNAME FROM USERS WHERE username = '"+username+"'";
+	connection.query(sql, function (err, results,fields) {  
+		if (results.length > 0) {
+			
+			response.send("Username already exists.");
+		} else {
+			sql = "INSERT INTO users (EMAIL,USERNAME,PASSWORD) VALUES ('"+email+"','"+username+"','"+password+"')";  
+			connection.query(sql, function (err, result) {  
+			if (err) throw err;  
+			console.log("1 record inserted");  
+			response.sendFile(path.join(__dirname + '/login.html'));
+			});  
+		}
+		response.end();
+		});  
+	
+	//response.sendFile(path.join(__dirname + '/login.html'));
+});
+app.get('/login', function(request, response) {
 	response.sendFile(path.join(__dirname + '/login.html'));
 });
-
 app.post('/auth', function(request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
 	if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		connection.query('SELECT USERNAME,PASSWORD FROM USERS WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
 			if (results.length > 0) {
 				request.session.loggedin = true;
 				request.session.username = username;
